f2ad2887eebbc17d174da5fbf70333ea4e9afff9
k1892085 <k1892085@kcl.ac.uk>
2020-01-19 13:34:55 +0000

    fixed colours and centered title

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -64,7 +64,8 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Fuck me')
+        centerTitle: true,
+        title: Text('EMBER')
       ),
       body: _children[_currentIndex],
         bottomNavigationBar: BottomNavigationBar(
