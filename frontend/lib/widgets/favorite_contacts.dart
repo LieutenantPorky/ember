846e3f02ec334744c4019d1da4503041ceffe103
k1892085 <k1892085@kcl.ac.uk>
2020-01-19 12:46:23 +0000

    some color fixes

--- frontend/lib/widgets/favorite_contacts.dart
+++ frontend/lib/widgets/favorite_contacts.dart
@@ -15,9 +15,9 @@ class FavoriteContacts extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
                 Text(
-                  'Favorite Contacts',
+                  'Matches',
                   style: TextStyle(
-                    color: Colors.blueGrey,
+                    color: Colors.white,
                     fontSize: 18.0,
                     fontWeight: FontWeight.bold,
                     letterSpacing: 1.0,
@@ -28,7 +28,7 @@ class FavoriteContacts extends StatelessWidget {
                     Icons.more_horiz,
                   ),
                   iconSize: 30.0,
-                  color: Colors.blueGrey,
+                  color: Colors.white,
                   onPressed: () {},
                 ),
               ],
@@ -63,7 +63,7 @@ class FavoriteContacts extends StatelessWidget {
                         Text(
                           favorites[index].name,
                           style: TextStyle(
-                            color: Colors.blueGrey,
+                            color: Colors.white,
                             fontSize: 16.0,
                             fontWeight: FontWeight.w600,
                           ),
